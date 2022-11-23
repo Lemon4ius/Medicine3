6040eab5239556ade903be731bb958d6ec9b2001
m_mar <vbvbyhhhgh@gmail.com>
2022-11-23 23:37:43 +0300

    Edit

--- app/src/main/java/com/example/medicine/Registrashion.java
+++ app/src/main/java/com/example/medicine/Registrashion.java
@@ -1,32 +1,16 @@
 package com.example.medicine;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.ContentValues;
-
-import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.SQLException;
-import android.database.sqlite.SQLiteCursor;
 import android.database.sqlite.SQLiteDatabase;
-
-import android.net.Uri;
-import android.net.ipsec.ike.exceptions.IkeNetworkLostException;
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
-import java.lang.invoke.ConstantCallSite;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Registrashion extends AppCompatActivity {
     Button check;
@@ -41,8 +25,6 @@ public class Registrashion extends AppCompatActivity {
         check = findViewById(R.id.enter);
         login = findViewById(R.id.Login);
         password = findViewById(R.id.password);
-
-
         Object_sqlite = new sqlite(this);
         sqLiteDatabase = Object_sqlite.getWritableDatabase();
 
@@ -77,13 +59,10 @@ public class Registrashion extends AppCompatActivity {
                     Toast.makeText(this, "Такой пароль уже существует", Toast.LENGTH_SHORT).show();
                     return;
                 }
-
-
             }
             sqLiteDatabase.insert(RegistrConst.SECONDTABLEUSERS, null, contentValues);
             Intent intent = new Intent(this, SingIn.class);
             startActivity(intent);
-
         }
     }
 
@@ -92,36 +71,5 @@ public class Registrashion extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         String pat = data.getData().getPath();
         System.out.println(pat);
-
-    }
-
-    public void Check(View view) {
-
-
-        List<String> lol = new ArrayList<>();
-        boolean log = false;
-
-        String Login = login.getText().toString();
-        String Password = password.getText().toString();
-        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM " + RegistrConst.SECONDTABLEUSERS + " WHERE " + RegistrConst.KEY_ID, null);
-
-        int nameindex = cursor.getColumnIndex(RegistrConst.KEY_LOGIN);
-        int passindex = cursor.getColumnIndex(RegistrConst.KEY_PASSWORD);
-        int idindex = cursor.getColumnIndex(RegistrConst.KEY_ID);
-
-        while (cursor.moveToNext()) {
-            if (Login.equals(cursor.getString(nameindex)) && Password.equals(cursor.getString(passindex))) {
-                check.setBackgroundColor(000000);
-
-
-            }
-
-            lol.add(cursor.getString(nameindex));
-            lol.add(cursor.getString(passindex));
-            for (String item : lol) {
-                System.out.println(item);
-            }
-        }
-        cursor.close();
     }
 }
